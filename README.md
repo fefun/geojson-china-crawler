b5a1e18be9298dc38a6a07954baa3297b55891de
qishanfeng <qishanfeng1987@gmail.com>
2018-09-07 11:35:22 +0800

    readme

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # 中国行政区geojson crawler
 
+
 如有问题，请发issue
 
+脚本使用于：
+
 数据源地址： https://lbs.amap.com/api/javascript-api/reference-amap-ui/geo/district-explorer  
+
+## 环境
+
+node 
+
+## 使用
+
+```
+npm i axios
+
+node crawler.js
+```
+
+
--- crawler.js
+++ crawler.js
@@ -6,11 +6,9 @@ async function getByCode (code) {
   let url = `https://webapi.amap.com/ui/1.0/ui/geo/DistrictExplorer/assets/d_v1/an_${code}.json?v=1.0.11&key=160cab8ad6c50752175d76e61ef92c50`
   return new Promise(function (resolve, reject) {
     axios.get(url).then(res => {
-      console.log(res.data)
       if (res.data) {
         let json = getFeatures(code, res.data)
-        console.log(json)
-        fs.writeFileSync('./geo/' + code + '.geojson', JSON.stringify(json))
+        fs.writeFileSync('./' + code + '.geojson', JSON.stringify(json))
         resolve(json)
       }
     })
@@ -19,4 +17,9 @@ async function getByCode (code) {
 
 ;(async function (params) {
   let geo = await getByCode('100000')
+  geo.features.forEach(async function (g) {
+    // TODO 
+    // let code = g.properties.adcode
+    // await getByCode(code)
+  })
 })()
